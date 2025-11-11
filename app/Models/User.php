f49b1ca42dbea042ee4377da658a9b4e9164d3c1
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2025-11-11 16:26:15 +0700

    Add customer type lists display user name

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,6 +71,11 @@ class User extends Authenticatable
         return $this->hasMany(Application::class, 'created_user_id');
     }
 
+    public function customerTypes()
+    {
+        return $this->hasMany(CustomerType::class, 'created_user_id');
+    }
+
     // Created by Sun 20/10/2025
     // public function createdCustomers()
     // {
