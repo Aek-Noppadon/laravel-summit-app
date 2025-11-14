28f2ef5128c97dd364ce8c58ba2ef5fdb472f1f4
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2025-11-14 11:08:10 +0700

    Add code fillable to Model Volume Unit

--- app/Models/VolumeUnit.php
+++ app/Models/VolumeUnit.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class VolumeUnit extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'created_user_id',
+        'updated_user_id',
+    ];
 }
