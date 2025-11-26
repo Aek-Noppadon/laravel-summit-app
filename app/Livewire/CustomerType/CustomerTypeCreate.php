162dce9b0a796ba6150bcc6170a7237f7574d56c
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2025-11-26 14:53:47 +0700

    Add code CustomerTypeCreate check empty department before save

--- app/Livewire/CustomerType/CustomerTypeCreate.php
+++ app/Livewire/CustomerType/CustomerTypeCreate.php
@@ -21,54 +21,55 @@ class CustomerTypeCreate extends Component
 
         $departmentId = auth()->user()->department_id;
 
-        $exists = CustomerType::where('name', $this->name)
-            ->whereHas('userCreated', function ($query) use ($departmentId) {
-                $query->where('department_id', $departmentId);
-            })->exists();
+        if (empty($departmentId)) {
+            $this->dispatch(
+                "sweet.error",
+                position: "center",
+                title: "Unable to add customer type !!",
+                text: "Please fill in the department.",
+                icon: "error",
+                url: route('user.profile'),
+            );
+        } else {
+            $exists = CustomerType::where('name', $this->name)
+                ->whereHas('userCreated', function ($query) use ($departmentId) {
+                    $query->where('department_id', $departmentId);
+                })->exists();
 
-        if ($exists) {
-            $this->addError('name', 'The customer type name has already been taken.');
-            return;
+            if ($exists) {
+                $this->addError('name', 'The customer type name has already been taken.');
+                return;
+            }
+
+            $this->validate(
+                [
+                    'name' => 'required',
+                ],
+                [
+                    'required' => 'The customer type :attribute field is required.',
+                ]
+            );
+
+            CustomerType::create(
+                [
+                    'name' => $this->name,
+                    'created_user_id' => auth()->user()->id,
+                    'updated_user_id' => auth()->user()->id,
+                ]
+            );
+
+            $this->dispatch(
+                "sweet.success",
+                position: "center",
+                title: "Created Successfully !!",
+                text: "Customer Type : " . $this->name,
+                icon: "success",
+                timer: 3000,
+                // url: route('customer-type.list'),
+            );
+
+            $this->dispatch('close-modal-customer-type');
         }
-
-        $this->validate(
-            [
-                'name' => 'required',
-            ],
-            [
-                'required' => 'The customer type :attribute field is required.',
-            ]
-        );
-
-        // $this->validate(
-        //     [
-        //         'name' => 'required|unique:customer_types',
-        //     ],
-        //     [
-        //         'required' => 'The customer type :attribute field is required !!',
-        //         'unique' => 'The customer type :attribute has already been taken !!',
-        //     ]
-        // );
-
-        CustomerType::create(
-            [
-                'name' => $this->name,
-                'created_user_id' => auth()->user()->id,
-                'updated_user_id' => auth()->user()->id,
-            ]
-        );
-
-        $this->dispatch(
-            "sweet.success",
-            position: "center",
-            title: "Created Successfully !!",
-            text: "Customer Type : " . $this->name,
-            icon: "success",
-            timer: 3000,
-            // url: route('customer-type.list'),
-        );
-
-        $this->dispatch('close-modal-customer-type');
     }
 
     #[On('reset-modal')]
