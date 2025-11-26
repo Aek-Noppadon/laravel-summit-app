b544fc75027a689fac60fa8b362f3407ee177b3f
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2025-11-26 15:00:53 +0700

    Add code ApplicationCreate check empty department before save

--- app/Livewire/Crm/Application/ApplicationCreate.php
+++ app/Livewire/Crm/Application/ApplicationCreate.php
@@ -21,43 +21,54 @@ class ApplicationCreate extends Component
 
         $departmentId = auth()->user()->department_id;
 
-        $exists = Application::where('name', $this->name)
-            ->whereHas('userCreated', function ($query) use ($departmentId) {
-                $query->where('department_id', $departmentId);
-            })->exists();
+        if (empty($departmentId)) {
+            $this->dispatch(
+                "sweet.error",
+                position: "center",
+                title: "Unable to add application !!",
+                text: "Please fill in the department.",
+                icon: "error",
+                url: route('user.profile'),
+            );
+        } else {
+            $exists = Application::where('name', $this->name)
+                ->whereHas('userCreated', function ($query) use ($departmentId) {
+                    $query->where('department_id', $departmentId);
+                })->exists();
 
-        if ($exists) {
-            $this->addError('name', 'The application name has already been taken.');
-            return;
+            if ($exists) {
+                $this->addError('name', 'The application name has already been taken.');
+                return;
+            }
+
+            $this->validate(
+                [
+                    'name' => 'required',
+                ],
+                [
+                    'required' => 'The application :attribute field is required.',
+                ]
+            );
+
+            Application::create(
+                [
+                    'name' => $this->name,
+                    'created_user_id' => auth()->user()->id,
+                    'updated_user_id' => auth()->user()->id,
+                ]
+            );
+
+            $this->dispatch(
+                "sweet.success",
+                position: "center",
+                title: "Created Successfully !!",
+                text: "Application : " . $this->name,
+                icon: "success",
+                timer: 3000,
+            );
+
+            $this->dispatch('close-modal-application');
         }
-
-        $this->validate(
-            [
-                'name' => 'required',
-            ],
-            [
-                'required' => 'The application :attribute field is required.',
-            ]
-        );
-
-        Application::create(
-            [
-                'name' => $this->name,
-                'created_user_id' => auth()->user()->id,
-                'updated_user_id' => auth()->user()->id,
-            ]
-        );
-
-        $this->dispatch(
-            "sweet.success",
-            position: "center",
-            title: "Created Successfully !!",
-            text: "Application : " . $this->name,
-            icon: "success",
-            timer: 3000,
-        );
-
-        $this->dispatch('close-modal-application');
     }
 
     #[On('reset-modal')]
