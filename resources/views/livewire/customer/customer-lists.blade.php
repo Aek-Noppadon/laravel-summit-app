1a3391487255aaab24c618ee6a8b1d404658a97a
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-01-06 16:24:40 +0700

    Edit code view customer

--- resources/views/livewire/customer/customer-lists.blade.php
+++ resources/views/livewire/customer/customer-lists.blade.php
@@ -105,7 +105,7 @@
                                     </small>
                                 </td>
                                 <td>
-                                    @if ($item->source === 0)
+                                    @if ($item->source === '0')
                                         <span class="badge badge-pill badge-info">
                                             AX
                                         </span>
@@ -117,7 +117,7 @@
                                 <td>{{ $item->name_thai }}</td>
                                 <td>{{ $item->parent_code }}</td>
                                 <td>
-                                    @if ($item->source === 1)
+                                    @if ($item->source === '1')
                                         <button
                                             wire:click.prevent="$dispatch('edit-customer',{id:{{ $item->id }}})"
                                             type="button" class="btn btn-secondary btn-sm" data-toggle="modal"
