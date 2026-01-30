cadcee6913724c08c309c2b991fe0ff1305f10fa
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-01-30 07:51:23 +0700

    Add code database model module event

---
+++ app/Models/Event.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'name',
+        'created_user_id',
+        'updated_user_id',
+    ];
+
+    public function userCreated()
+    {
+        return $this->belongsTo(User::class, 'created_user_id');
+    }
+
+    public function userUpdated()
+    {
+        return $this->belongsTo(User::class, 'updated_user_id');
+    }
+}
