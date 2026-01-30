9ab45b8877d00c5676c7f0cf945693bdd535a3f1
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-01-30 07:54:16 +0700

    Edit code livewire view layout footer

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -1,7 +1,9 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+<@php
+    $databaseName = env('DB_DATABASE');
+@endphp <footer class="main-footer">
+    <strong>Copyright &copy; 2026 Summit Web Applications</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
+        <b>Connection to</b> {{ $databaseName }}
     </div>
-</footer>
+    </footer>
