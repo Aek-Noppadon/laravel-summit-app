fb91dc6061197ef524a369c8ca85f98ac09c84e0
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-02-05 13:48:31 +0700

    Edit code livewire view  sales stage

--- resources/views/livewire/sales-stage/sales-stage-create.blade.php
+++ resources/views/livewire/sales-stage/sales-stage-create.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Add Sales Stage</h4>
+                <h4 class="modal-title">Sales Stage</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -11,7 +11,7 @@
             <div class="modal-body">
                 <div class="card card-primary">
                     <div class="card-header">
-                        <h3 class="card-title">Sales Stage Form</h3>
+                        <h3 class="card-title">Add Form</h3>
                     </div>
                     <form>
                         <div class="card-body">
