9f1f628f781eeea45636b5fcdba5a9668810926f
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-02-05 13:45:15 +0700

    Edit code livewire view  customer type

--- resources/views/livewire/crm/customer-type/customer-type-edit.blade.php
+++ resources/views/livewire/crm/customer-type/customer-type-edit.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Edit Customer Type</h4>
+                <h4 class="modal-title">Customer Type</h4>
                 <button wire:click="$dispatch('close-modal')" type="button" class="close" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -14,7 +14,7 @@
             <div class="modal-body">
                 <div class="card card-primary">
                     <div class="card-header">
-                        <h3 class="card-title">Customer Type Form</h3>
+                        <h3 class="card-title">Edit Form</h3>
                     </div>
 
                     <form>
