c118035fc2393f89cf3aac3948453f7cb1bd00f6
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-02-05 13:49:29 +0700

    Edit code livewire view  user profile

--- resources/views/livewire/user/user-profile.blade.php
+++ resources/views/livewire/user/user-profile.blade.php
@@ -6,11 +6,11 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Edit User Profile</h1>
+                        <h1>User Profile</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
                             <li class="breadcrumb-item active">Profile</li>
                         </ol>
                     </div>
@@ -18,13 +18,12 @@
             </div><!-- /.container-fluid -->
         </section>
     </div>
-
     <!-- card -->
 
 
     <div class="card card-primary">
         <div class="card-header">
-            <h3 class="card-title">Form</h3>
+            <h3 class="card-title">Edit Form</h3>
         </div>
 
         <form>
