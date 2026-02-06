8edcfa02f1e8b41c3b379be2c6f4533d586ebc93
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-02-06 10:00:11 +0700

    Edit code view livewire layout footer

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -1,9 +1,11 @@
-<@php
+@php
     $databaseName = env('DB_DATABASE');
-@endphp <footer class="main-footer">
+@endphp
+
+<footer class="main-footer">
     <strong>Copyright &copy; 2026 Summit Web Applications</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Connection to</b> {{ $databaseName }}
     </div>
-    </footer>
+</footer>
