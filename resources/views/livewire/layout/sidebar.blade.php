67fbc2eb29c4fb8a02ed437773e1cd5f0b1ac922
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-02-27 21:13:05 +0700

    Edit view livewire layout sidebar

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
-        <img src="{{ asset('images/logo/favicon.png') }}"Logo" class="brand-image">
+        <img src="{{ asset('images/logo/favicon.png') }}" Logo" class="brand-image">
         <span class="brand-text font-weight-light">Summit Chemical</span>
     </a>
 
@@ -15,9 +15,9 @@
                     {{ Auth::user()->last_name }}</a>
 
                 @isset(auth()->user()->department->name)
-                    <div class="d-block text-light">
-                        {{ auth()->user()->department->name }}
-                    </div>
+                <div class="d-block text-light">
+                    {{ auth()->user()->department->name }}
+                </div>
                 @endisset
             </div>
         </div>
@@ -25,8 +25,7 @@
         <!-- SidebarSearch Form -->
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                    aria-label="Search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
                 <div class="input-group-append">
                     <button class="btn btn-sidebar">
                         <i class="fas fa-search fa-fw"></i>
@@ -37,12 +36,10 @@
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-                <!-- User Menu -->
-                <li class="nav-item {{ request()->is('user*', 'department*') ? 'menu-open' : '' }}">
-                    <a href="#" class="nav-link {{ request()->is('user*', 'department*') ? 'active' : '' }}">
+                <li class="nav-item {{ request()->is('user*', 'department*', 'role*') ? 'menu-open' : '' }}">
+                    <a href="#" class="nav-link {{ request()->is('user*', 'department*', 'role*') ? 'active' : '' }}">
                         <i class="fas fa-users-cog"></i>
                         <p>
                             User
@@ -50,40 +47,54 @@
                         </p>
                     </a>
 
-                    <!-- User Profile -->
                     <ul class="nav nav-treeview">
+                        <!-- User Profile -->
                         <li class="nav-item">
                             <a href="{{ route('user.profile') }}"
-                                class="nav-link {{ request()->is('user/profile') ? 'active' : '' }}">
+                                class="nav-link {{ request()->is('users/profile') ? 'active' : '' }}">
                                 <i class="far fa-address-card nav-icon"></i>
                                 <p>Profile</p>
                             </a>
+                        </li>
+
+                        <!-- Role Lists -->
+                        @if (auth()->user()->can('role.view'))
+                        <li class="nav-item">
+                            <a href="{{ route('role.list') }}"
+                                class="nav-link {{ request()->is('role*') ? 'active' : '' }}">
+                                <i class="fas fa-chevron-circle-down nav-icon"></i>
+                                <p>Role Lists</p>
+                            </a>
+                        </li>
+                        @endif
+
+                        <!-- User Lists -->
+                        @if (auth()->user()->can('user.view'))
+                        <li class="nav-item">
                             <a href="{{ route('user.list') }}"
-                                class="nav-link {{ request()->is('user/list') ? 'active' : '' }}">
-                                <i class="far fa-address-card nav-icon"></i>
+                                class="nav-link {{ request()->is('users') ? 'active' : '' }}">
+                                <i class="fas fa-chevron-circle-down nav-icon"></i>
                                 <p>User Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endcan
 
-                    <!-- Department Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Department Lists -->
+                        @if (auth()->user()->can('department.view'))
                         <li class="nav-item">
                             <a href="{{ route('department.list') }}"
-                                class="nav-link {{ request()->is('department*') ? 'active' : '' }}">
+                                class="nav-link {{ request()->is('departments') ? 'active' : '' }}">
                                 <i class="fas fa-chevron-circle-down nav-icon"></i>
-                                <p>Department</p>
+                                <p>Department Lists</p>
                             </a>
                         </li>
+                        @endcan
                     </ul>
                 </li>
 
-                <!-- CRM Menu -->
-                <li
-                    class="nav-item {{ request()->is('crms*', 'customers*', 'products*', 'applications*', 'customer-types*', 'customer-groups*', 'sales-stages*', 'probabilities*', 'volume-units*', 'events*') ? 'menu-open' : '' }}">
-                    <a href="#"
-                        class="nav-link {{ request()->is('crms*', 'customers*', 'products*', 'applications*', 'customer-types*', 'customer-groups*', 'sales-stages*', 'probabilities*', 'volume-units*', 'events*') ? 'active' : '' }}">
-                        {{-- <i class="fab fa-wordpress-simple nav-icon"></i> --}}
+                @if (auth()->user()->can('crm.view') || auth()->user()->can('crmDelete.view'))
+                <li class="nav-item {{ request()->is('crms*') ? 'menu-open' : '' }}">
+                    <a href="#" class="nav-link {{ request()->is('crms*') ? 'active' : '' }}">
                         <i class="fas fa-bars nav-icon"></i>
                         <p>
                             CRM Application
@@ -91,29 +102,51 @@
                         </p>
                     </a>
 
-                    <!-- CRM Lists -->
                     <ul class="nav nav-treeview">
+                        <!-- CRM Lists -->
+                        @if (auth()->user()->can('crm.view'))
                         <li class="nav-item">
                             <a href="{{ route('crm.list') }}"
-                                class="nav-link {{ request()->is('crms/lists', 'crms/create', 'crms/update*') ? 'active' : '' }}">
+                                class="nav-link {{ request()->is('crms', 'crms/create', 'crms/update*') ? 'active' : '' }}">
                                 <i class="fas fa-chevron-circle-down nav-icon"></i>
-                                {{-- <i class="far fa-circle nav-icon"></i> --}}
                                 <p>CRM Lists</p>
                             </a>
                         </li>
+                        @endif
 
+                        <!-- CRM Delete Lists -->
+                        @if (auth()->user()->can('crmDelete.view'))
                         <li class="nav-item">
                             <a href="{{ route('crm.list.delete') }}"
-                                class="nav-link {{ request()->is('crms/lists/delete') ? 'active' : '' }}">
+                                class="nav-link {{ request()->is('crms/delete') ? 'active' : '' }}">
                                 <i class="fas fa-chevron-circle-down nav-icon"></i>
-                                {{-- <i class="far fa-circle nav-icon"></i> --}}
                                 <p>CRM Delete Lists</p>
                             </a>
                         </li>
+                        @endif
                     </ul>
+                </li>
+                @endif
 
-                    <!-- Customer Lists -->
+                @if (auth()->user()->can('customer.view') ||
+                auth()->user()->can('customerType.view') ||
+                auth()->user()->can('customerGroup.view') ||
+                auth()->user()->can('event.view'))
+                <li
+                    class="nav-item {{ request()->is('customers*', 'customer-types*', 'customer-groups*', 'events*') ? 'menu-open' : '' }}">
+                    <a href="#"
+                        class="nav-link {{ request()->is('customers*', 'customer-types*', 'customer-groups*', 'events*') ? 'active' : '' }}">
+                        <i class="fas fa-bars nav-icon"></i>
+                        <p>
+                            Customer Masters
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+
                     <ul class="nav nav-treeview">
+
+                        <!-- Customer Lists -->
+                        @if (auth()->user()->can('customer.view'))
                         <li class="nav-item">
                             <a href="{{ route('customer.list') }}"
                                 class="nav-link {{ request()->is('customers*') ? 'active' : '' }}">
@@ -121,21 +154,10 @@
                                 <p>Customer Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endif
 
-                    <!-- Event Lists -->
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('event.list') }}"
-                                class="nav-link {{ request()->is('events*') ? 'active' : '' }}">
-                                <i class="fas fa-chevron-circle-down nav-icon"></i>
-                                <p>Event Lists</p>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <!-- Customer Type Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Customer Type Lists -->
+                        @if (auth()->user()->can('customerType.view'))
                         <li class="nav-item">
                             <a href="{{ route('customer-type.list') }}"
                                 class="nav-link {{ request()->is('customer-types*') ? 'active' : '' }}">
@@ -143,10 +165,10 @@
                                 <p>Customer Type Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endcan
 
-                    <!-- Customer Group Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Customer Group Lists -->
+                        @if (auth()->user()->can('customerGroup.view'))
                         <li class="nav-item">
                             <a href="{{ route('customer-group.list') }}"
                                 class="nav-link {{ request()->is('customer-groups*') ? 'active' : '' }}">
@@ -154,10 +176,44 @@
                                 <p>Customer Group Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endcan
 
-                    <!-- Product Lists -->
+                        <!-- Event Lists -->
+                        @if (auth()->user()->can('event.view'))
+                        <li class="nav-item">
+                            <a href="{{ route('event.list') }}"
+                                class="nav-link {{ request()->is('events*') ? 'active' : '' }}">
+                                <i class="fas fa-chevron-circle-down nav-icon"></i>
+                                <p>Event Lists</p>
+                            </a>
+                        </li>
+                        @endcan
+
+                    </ul>
+                </li>
+                @endif
+
+                @if (auth()->user()->can('product.view') ||
+                auth()->user()->can('application.view') ||
+                auth()->user()->can('salesStage.view') ||
+                auth()->user()->can('probability.view') ||
+                auth()->user()->can('volumeUnit.view') ||
+                auth()->user()->can('event.view'))
+                <li
+                    class="nav-item {{ request()->is('products*', 'applications*', 'sales-stages*', 'probabilities*', 'volume-units*') ? 'menu-open' : '' }}">
+                    <a href="#"
+                        class="nav-link {{ request()->is('products*', 'applications*', 'sales-stages*', 'probabilities*', 'volume-units*') ? 'active' : '' }}">
+                        <i class="fas fa-bars nav-icon"></i>
+                        <p>
+                            Data Masters
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+
                     <ul class="nav nav-treeview">
+
+                        <!-- Product Lists -->
+                        @if (auth()->user()->can('product.view'))
                         <li class="nav-item">
                             <a href="{{ route('product.list') }}"
                                 class="nav-link {{ request()->is('products*') ? 'active' : '' }}">
@@ -165,10 +221,10 @@
                                 <p>Product Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endif
 
-                    <!-- Application Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Application Lists -->
+                        @if (auth()->user()->can('application.view'))
                         <li class="nav-item">
                             <a href="{{ route('application.list') }}"
                                 class="nav-link {{ request()->is('applications*') ? 'active' : '' }}">
@@ -176,10 +232,10 @@
                                 <p>Application Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endif
 
-                    <!-- Probability Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Probability Lists -->
+                        @if (auth()->user()->can('probability.view'))
                         <li class="nav-item">
                             <a href="{{ route('probability.list') }}"
                                 class="nav-link {{ request()->is('probabilities*') ? 'active' : '' }}">
@@ -187,10 +243,10 @@
                                 <p>Probability Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endif
 
-                    <!-- Volume Unit Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Volume Unit Lists -->
+                        @if (auth()->user()->can('volumeUnit.view'))
                         <li class="nav-item">
                             <a href="{{ route('volume-unit.list') }}"
                                 class="nav-link {{ request()->is('volume-units*') ? 'active' : '' }}">
@@ -198,10 +254,10 @@
                                 <p>Volume Unit Lists</p>
                             </a>
                         </li>
-                    </ul>
+                        @endif
 
-                    <!-- Sales Stage Lists -->
-                    <ul class="nav nav-treeview">
+                        <!-- Sales Stage Lists -->
+                        @if (auth()->user()->can('salesStage.view'))
                         <li class="nav-item">
                             <a href="{{ route('sales-stage.list') }}"
                                 class="nav-link {{ request()->is('sales-stages*') ? 'active' : '' }}">
@@ -209,8 +265,14 @@
                                 <p>Sales Stage Lists</p>
                             </a>
                         </li>
+
+                        @endif
+
                     </ul>
+
                 </li>
+                @endif
+
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
