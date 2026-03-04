e6e530989105c055b4fc833593f1a3dbbb5cdbdd
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-03-04 09:26:09 +0700

    Create code livewire Crm CrmListCloseDiscontunued

---
+++ app/Livewire/Crm/CrmListCloseDiscontinued.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Livewire\Crm;
+
+use App\Models\CrmDetail;
+use App\Models\CrmHeader;
+use Carbon\Carbon;
+use Livewire\Attributes\On;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class CrmListCloseDiscontinued extends Component
+{
+    use WithPagination;
+    public $pagination = 20;
+    public $isOpenId = null;
+    public $isOpenSearch = true;
+    public $yearAgo, $userId;
+
+    #[On('refresh-crm')]
+    public function render()
+    {
+        $this->yearAgo = Carbon::now()->subYear(2);
+
+        $this->userId = auth()->user()->id;
+
+        $crms = CrmHeader::where(function ($query) {
+            $query->where('created_user_id', $this->userId);
+        })->where(function ($query) {
+            $query->where('estimate_date', '<', $this->yearAgo);
+        })
+            ->whereHas('crm_items', function ($query) {
+                $query->whereIn('sales_stage_id', [1, 2, 3]);
+            })
+            ->withCount('crm_items')
+            ->orderByDesc('document_no')
+            // ->orderByDesc('estimate_date')
+            ->paginate($this->pagination);
+
+        return view('livewire.crm.crm-list-close-discontinued', compact('crms'));
+    }
+
+    public function toggleSearch($value)
+    {
+        $this->isOpenSearch = ($this->isOpenSearch === $value) ? null : $value;
+
+        $this->isOpenId = null;
+    }
+
+    public function toggle($id)
+    {
+        $this->isOpenId = ($this->isOpenId === $id) ? null : $id;
+
+        $this->isOpenSearch = true;
+    }
+
+    public function crmDiscontinued($id, $document_no, $name_english)
+    {
+        $this->dispatch("confirm", id: $id, document_no: $document_no, name_english: $name_english);
+    }
+
+    #[On('discontinued')]
+    public function discontinued($id, $document_no, $name_english)
+    {
+
+        CrmDetail::where('crm_id', $id)
+            ->whereIn('sales_stage_id', [1, 2, 3])
+            ->update([
+                'updated_visit_date' => now()->format('Y-m-d'),
+                'sales_stage_id' => 6
+            ]);
+
+        // Loop through the collection to save each model individually 
+        // $crmDetails = CrmDetail::where('crm_id', $id)
+        //     ->whereIn('sales_stage_id', [1, 2, 3])
+        //     ->get();
+
+        // foreach ($crmDetails as $item) {
+        //     $item->updated_visit_date = now()->format('Y-m-d');
+        //     $item->sales_stage_id = 6;
+        //     $item->save();
+        // }
+
+        $this->dispatch(
+            "sweet.success",
+            position: "center",
+            title: "Discontinued Successfully !!",
+            text: $document_no . " : " . $name_english,
+            icon: "success",
+            timer: 3000,
+            url: route('crm.list.close.discontinued'),
+        );
+    }
+}
