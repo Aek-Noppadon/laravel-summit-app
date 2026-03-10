29a21cf5ad40638f6f8a692cb3dd47ea6db6ed2b
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-03-10 09:41:53 +0700

    Create app livewire Ncp NcpCreate

---
+++ app/Livewire/Ncp/NcpCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Ncp;
+
+use Livewire\Component;
+
+class NcpCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.ncp.ncp-create');
+    }
+}
