d54ca0cd427754169b7025030bf12f9b740017e1
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-03-23 09:14:16 +0700

    Add Model FoundActivity

---
+++ app/Models/FoundActivity.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FoundActivity extends Model
+{
+    public $fillable = [
+        'name',
+        'created_user_id',
+        'updated_user_id',
+    ];
+
+    public function userCreated()
+    {
+        return $this->belongsTo(User::class, 'created_user_id');
+    }
+
+    public function userUpdated()
+    {
+        return $this->belongsTo(User::class, 'updated_user_id');
+    }
+}
