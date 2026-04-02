0cfbfde03acc50850edbff99ac4d505050cd395f
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-04-02 08:43:05 +0700

    Edit code Model Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
@@ -37,5 +38,52 @@ class Customer extends Model
     public function userUpdated()
     {
         return $this->belongsTo(User::class, 'updated_user_id');
+    }
+
+    protected function nameEnglish(): Attribute
+    {
+        /**
+         * จัดการข้อมูล name_english อัตโนมัติก่อนเข้า Database
+         */
+        return Attribute::make(
+            // ตอนดึงข้อมูลออกมา (Accessor) - ถ้าอยากให้พิมพ์ใหญ่ตอนโชว์ด้วย
+            get: fn(string $value) => strtoupper($value),
+
+            // ตอนเซตข้อมูล (Mutator) - หัวใจสำคัญที่คุณต้องการ
+            set: fn(string $value) => strtoupper(trim($value)),
+        );
+    }
+
+    protected function nameThai(): Attribute
+    {
+        /**
+         * จัดการข้อมูล name_thai อัตโนมัติก่อนเข้า Database
+         */
+        return Attribute::make(
+            // ตอนเซตข้อมูล (Mutator) - หัวใจสำคัญที่คุณต้องการ
+            set: fn(string $value) => trim($value),
+        );
+    }
+
+    protected function parentCode(): Attribute
+    {
+        /**
+         * จัดการข้อมูล name_thai อัตโนมัติก่อนเข้า Database
+         */
+        return Attribute::make(
+            // ตอนเซตข้อมูล (Mutator) - หัวใจสำคัญที่คุณต้องการ
+            set: fn(string $value) => trim($value),
+        );
+    }
+
+    protected function parentName(): Attribute
+    {
+        /**
+         * จัดการข้อมูล name_thai อัตโนมัติก่อนเข้า Database
+         */
+        return Attribute::make(
+            // ตอนเซตข้อมูล (Mutator) - หัวใจสำคัญที่คุณต้องการ
+            set: fn(string $value) => trim($value),
+        );
     }
 }
