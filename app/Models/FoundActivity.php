3207cf6092d4792b7af8bfeafd845161b1340251
Aek-Noppadon <131841201+Aek-Noppadon@users.noreply.github.com>
2026-04-02 08:43:30 +0700

    Edit code Model FoundActivity

--- app/Models/FoundActivity.php
+++ app/Models/FoundActivity.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class FoundActivity extends Model
@@ -20,5 +21,19 @@ class FoundActivity extends Model
     public function userUpdated()
     {
         return $this->belongsTo(User::class, 'updated_user_id');
+    }
+
+    protected function name(): Attribute
+    {
+        /**
+         * จัดการข้อมูล name อัตโนมัติก่อนเข้า Database
+         */
+        return Attribute::make(
+            // ตอนดึงข้อมูลออกมา (Accessor) - ถ้าอยากให้พิมพ์ใหญ่ตอนโชว์ด้วย
+            get: fn(string $value) => ucwords($value),
+
+            // ตอนเซตข้อมูล (Mutator) - หัวใจสำคัญที่คุณต้องการ
+            set: fn(string $value) => ucwords(trim($value)),
+        );
     }
 }
